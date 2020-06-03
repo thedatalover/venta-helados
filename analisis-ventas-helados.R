f73c74059897769507e13c53df23f9ff04bb0a03
Jose Arturo Mora Soto <jarturomora@trilogyed.com>
2020-06-03 17:05:56 -0500

    Agrega gráfico de líneas

--- analisis-ventas-helados.R
+++ analisis-ventas-helados.R
@@ -10,3 +10,24 @@ head(datos_ventas, 10)
 
 # Muestra las principales estadísticas descriptivas del Data Frame
 summary(datos_ventas)
+
+####################################
+# Análisis Preliminar de los Datos #
+####################################
+
+# Crea un gráfico de líneas con las ventas a lo largo de los 30 días
+ventas <- datos_ventas$ventas
+plot(
+  ganancias,
+  type = "o",
+  col = "blue",
+  ylim = c(0, 200)
+  )
+
+# Agrega una línea con el total de ventas por día
+ganancias <- datos_ventas$ganancia
+lines(
+  ventas,
+  type = "o",
+  col = "red"
+)
